3dcf1135e79587e653c505bf986c399d9f65c8ea
Luan Aguiar <luanaguiar16@live.com>
2018-12-05 15:04:12 -0200

    meatapp - antes de implementar dinamica de restaurantes

---
+++ src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import {Routes} from '@angular/router'
+
+import {HomeComponent} from './home/home.component'
+import {AboutComponent} from './about/about.component'
+import {RestaurantesComponent} from './restaurantes/restaurantes.component'
+
+export const ROUTES: Routes = [
+
+    {path: '', component: HomeComponent},
+    {path: 'restaurantes', component: RestaurantesComponent},
+    {path: 'about', component: AboutComponent}
+    
+]
