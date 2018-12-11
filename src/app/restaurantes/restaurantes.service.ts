6dd123a0ebbc35f7cf77f7e7c4e3b7fb3e718150
Luan Aguiar <luanaguiar16@live.com>
2018-12-11 16:14:49 -0200

    Ultima atualização

--- src/app/restaurantes/restaurantes.service.ts
+++ src/app/restaurantes/restaurantes.service.ts
@@ -3,15 +3,17 @@ import { Http } from '@angular/http'
 import { Observable } from 'rxjs/Observable'
 import { Restaurant } from './restaurant/restaurant.model'
 import { MEAT_API } from '../app.api'
+import { ErrorHandler } from '../app.error-handler'
 import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
 
-
+@Injectable()
 export class RestaurantsService{
 
     constructor(private http: Http){}
     
     restaurants(): Observable<Restaurant[]> {
-       return this.http.get(`${MEAT_API}/restaurants`).map(response => response.json())
+       return this.http.get(`${MEAT_API}/restaurants`).map(response => response.json()).catch(ErrorHandler.handleErro)
     }
 
 }
